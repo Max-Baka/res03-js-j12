24073ec19293c53e2ab356fba3356805dad9daa6
Max-Baka <metal-smaks@hotmail.fr>
2023-01-18 14:27:44 +0000

    je suis un gland

---
+++ soutien/index.js
@@ -0,0 +1,76 @@
+/*let user = {
+firstName : "Yeppy",
+lastName : "Yep",
+};
+user.firstName = "Yeppidy";
+console.log(user.firstName);
+console.log(user.lastName);
+console.log(user.firstName);*/
+
+/*let users = [
+    {
+        firstName : "Teddy",
+        lastName : "Roosevelt"
+    },
+    {
+        firstName : "Barack",
+        lastName : "Obama"
+    },
+    {
+        firstName : "Donald",
+        lastName : "Trump"
+    }
+];
+
+for(user of users){
+    console.log(user.firstName);
+}*/
+
+
+let pizzas = [
+    {
+        name : "Veggie",
+        ingredients : [
+            "Tomato",
+            "Cheese",
+            "Mushrooms"
+        ],
+        price : null
+    },
+    {
+        name : "Meattie",
+        ingredients : [
+            "Tomato",
+            "Cheese",
+            "Meat"
+        ],
+        price : null
+    },
+    {
+        name : "Fishie",
+        ingredients : [
+            "Tomato",
+            "Cheese",
+            "Fish"
+        ],
+        price : null
+    },
+    {
+        name : "Supreme",
+        ingredients : [
+            "Tomato",
+            "Cheese",
+            "Meat",
+            "Olive"
+        ],
+        price : null
+    }
+];
+for(let i = 0; i < pizzas.length; i++){
+   
+   for(let i= 0, ingredients.length; i++){
+       console.log(ingrdients[i]);
+   }
+    }
+    
+}
